ecbdf00579c3342ab0b07b51332f5fd53f607365
Markus Piipari <markus@cosmicjetpack.com>
2019-10-06 22:55:38 +0300

    Include HEIC as a supported image file format

--- Carpaccio/Image.swift
+++ Carpaccio/Image.swift
@@ -367,7 +367,7 @@ open class Image: Equatable, Hashable, CustomStringConvertible {
     }()
 
     public static var bakedImageFileExtensions: Set<String> = {
-        return Set(["jpg", "jpeg", "png", "tiff", "tif", "gif"])
+        return Set(["jpg", "jpeg", "png", "tiff", "tif", "gif", "heic"])
     }()
     
     public func hash(into hasher: inout Hasher) {
