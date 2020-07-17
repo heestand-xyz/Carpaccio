f85bf325def7311373b84e29e94dd170b9ce5192
Matias Piipari <matias.piipari@gmail.com>
2020-07-18 00:58:47 +0300

    Build Carpaccio as a dynamic library.

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Carpaccio",
+            type: .dynamic,
             targets: ["Carpaccio"]
         ),
         .executable(name: "exifdump", targets: ["exifdump"])
